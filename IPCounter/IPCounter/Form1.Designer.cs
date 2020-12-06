5e6ca3bde628391bd39cd555ffcdadb3956e270f
abstractspoon <daniel.godson@gmail.com>
2020-12-06 11:17:18 +1100

    Add IPCounter app

---
+++ IPCounter/IPCounter/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace IPCounter
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.m_BrowseButton = new System.Windows.Forms.Button();
+			this.m_LogFileList = new System.Windows.Forms.ListBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.m_ProgressList = new System.Windows.Forms.ListBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.m_ResultsList = new System.Windows.Forms.ListView();
+			this.IPAddress = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.IPCount = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.IPPercent = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.SuspendLayout();
+			// 
+			// m_BrowseButton
+			// 
+			this.m_BrowseButton.AutoSize = true;
+			this.m_BrowseButton.Location = new System.Drawing.Point(12, 12);
+			this.m_BrowseButton.Name = "m_BrowseButton";
+			this.m_BrowseButton.Size = new System.Drawing.Size(106, 23);
+			this.m_BrowseButton.TabIndex = 1;
+			this.m_BrowseButton.Text = "Browse Log Files...";
+			this.m_BrowseButton.UseVisualStyleBackColor = true;
+			this.m_BrowseButton.Click += new System.EventHandler(this.OnBrowseLogFiles);
+			// 
+			// m_LogFileList
+			// 
+			this.m_LogFileList.FormattingEnabled = true;
+			this.m_LogFileList.Location = new System.Drawing.Point(13, 42);
+			this.m_LogFileList.Name = "m_LogFileList";
+			this.m_LogFileList.Size = new System.Drawing.Size(285, 69);
+			this.m_LogFileList.TabIndex = 2;
+			// 
+			// button1
+			// 
+			this.button1.AutoSize = true;
+			this.button1.Location = new System.Drawing.Point(13, 130);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(99, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Analyse Log Files";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.OnAnalyseLogFiles);
+			// 
+			// m_ProgressList
+			// 
+			this.m_ProgressList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.m_ProgressList.FormattingEnabled = true;
+			this.m_ProgressList.Location = new System.Drawing.Point(12, 180);
+			this.m_ProgressList.Name = "m_ProgressList";
+			this.m_ProgressList.Size = new System.Drawing.Size(286, 121);
+			this.m_ProgressList.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(10, 164);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(48, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Progress";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(309, 26);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(42, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Results";
+			// 
+			// m_ResultsList
+			// 
+			this.m_ResultsList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.m_ResultsList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.IPAddress,
+            this.IPCount,
+            this.IPPercent});
+			this.m_ResultsList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+			this.m_ResultsList.Location = new System.Drawing.Point(312, 42);
+			this.m_ResultsList.Name = "m_ResultsList";
+			this.m_ResultsList.Size = new System.Drawing.Size(267, 259);
+			this.m_ResultsList.TabIndex = 5;
+			this.m_ResultsList.UseCompatibleStateImageBehavior = false;
+			this.m_ResultsList.View = System.Windows.Forms.View.Details;
+			// 
+			// IPAddress
+			// 
+			this.IPAddress.Text = "IP Address";
+			this.IPAddress.Width = 127;
+			// 
+			// IPCount
+			// 
+			this.IPCount.Text = "Count";
+			this.IPCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.IPCount.Width = 61;
+			// 
+			// IPPercent
+			// 
+			this.IPPercent.Text = "%";
+			this.IPPercent.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.IPPercent.Width = 36;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(591, 311);
+			this.Controls.Add(this.m_ResultsList);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.m_ProgressList);
+			this.Controls.Add(this.m_LogFileList);
+			this.Controls.Add(this.m_BrowseButton);
+			this.Name = "Form1";
+			this.Text = "IP Address Counter";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button m_BrowseButton;
+		private System.Windows.Forms.ListBox m_LogFileList;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ListBox m_ProgressList;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ListView m_ResultsList;
+		private System.Windows.Forms.ColumnHeader IPAddress;
+		private System.Windows.Forms.ColumnHeader IPCount;
+		private System.Windows.Forms.ColumnHeader IPPercent;
+	}
+}
+
---
+++ IPCounter/IPCounter/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IPCounter
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+// 			int colWidth = ((m_ResultsList.Width - SystemInformation.VerticalScrollBarWidth) / m_ResultsList.Columns.Count);
+// 
+// 			foreach (ColumnHeader col in m_ResultsList.Columns)
+// 				col.Width = colWidth;
+		}
+
+		private void OnBrowseLogFiles(object sender, EventArgs e)
+		{
+			var dlg = new OpenFileDialog
+			{
+				Title = "Select Compressed Apache Logs",
+
+				AutoUpgradeEnabled = true,
+				CheckFileExists = true,
+				CheckPathExists = true,
+				Multiselect = true,
+
+				InitialDirectory = "D:\\Downloads",
+				Filter = "Compressed Logs (*.gz)|abstractspoon.com-*.gz||",
+				RestoreDirectory = true,
+
+				ShowReadOnly = true
+			};
+
+			if (dlg.ShowDialog() == DialogResult.OK)
+			{
+				m_LogFileList.Items.Clear();
+				m_ProgressList.Items.Clear();
+				m_ResultsList.Items.Clear();
+
+				foreach (var logPath in dlg.FileNames)
+				{
+					m_LogFileList.Items.Add(logPath);
+				}
+			}
+		}
+
+		private void OnAnalyseLogFiles(object sender, EventArgs e)
+		{
+			var ipCounts = new Dictionary<string, int>();
+			int totalCount = 0;
+
+			foreach (var logPath in m_LogFileList.Items)
+			{
+				m_ProgressList.Items.Add(string.Format("Unzipping '{0}'", logPath.ToString()));
+				m_ProgressList.Update();
+
+				var tempPath = UnzipLogFile(logPath.ToString());
+
+				if (!string.IsNullOrEmpty(tempPath))
+				{
+					m_ProgressList.Items.Add(string.Format("Analysing '{0}'", logPath.ToString()));
+					m_ProgressList.Update();
+					
+					totalCount = AnalyseLogFile(tempPath, ipCounts);
+				}
+			}
+
+			// Sort and write results 
+			ipCounts = ipCounts.OrderByDescending(i => i.Value).ToDictionary(i => i.Key, i => i.Value);
+
+			m_ResultsList.SuspendLayout();
+
+			foreach (var ip in ipCounts)
+			{
+				var lvItem = new ListViewItem(ip.Key);
+
+				lvItem.SubItems.Add(ip.Value.ToString());
+				lvItem.SubItems.Add(string.Format("{0:F1}", ((ip.Value * 100.0) / totalCount)));
+
+				m_ResultsList.Items.Add(lvItem);
+			}
+
+			m_ResultsList.ResumeLayout();
+		}
+
+		private int AnalyseLogFile(string logPath, Dictionary<string, int> ipCounts)
+		{
+			int numLines = 0;
+
+			try
+			{
+				var file = new System.IO.StreamReader(logPath);
+				string line;
+
+				while ((line = file.ReadLine()) != null)
+				{
+					numLines++;
+					var ipAddress = line.Substring(0, line.IndexOf(' '));
+
+					if (!string.IsNullOrEmpty(ipAddress))
+					{
+						if (!ipCounts.ContainsKey(ipAddress))
+							ipCounts[ipAddress] = 0;
+
+						ipCounts[ipAddress]++;
+					}
+				}
+			}
+			catch (Exception)
+			{
+				ipCounts.Clear();
+				numLines = 0;
+			}
+
+			return numLines;
+		}
+
+		//////////////////////////////////////////////////////////////////////////////////////
+		// Decompressing Apache logs containing multiple chunks courtesy of:
+		// https://bamcisnetworks.wordpress.com/2017/05/22/decompressing-concatenated-gzip-files-in-c-received-from-aws-cloudwatch-logs/
+		//////////////////////////////////////////////////////////////////////////////////////
+
+		private static string UnzipLogFile(string logPath)
+		{
+			// Get the bytes of the file
+			byte[] FileBytes = File.ReadAllBytes(logPath);
+
+			List<int> StartIndexes = new List<int>();
+
+			/*
+			* This pattern indicates the start of a GZip file as found from looking at the files
+			* The file header is 10 bytes in size
+			* 0-1 Signature 0x1F, 0x8B
+			* 2 Compression Method - 0x08 is for DEFLATE, 0-7 are reserved
+			* 3 Flags
+			* 4-7 Last Modification Time
+			* 8 Compression Flags
+			* 9 Operating System
+			*/
+
+			byte[] StartOfFilePattern = new byte[] { 0x1F, 0x8B, 0x08 };
+
+			//This will limit the last byte we check to make sure it doesn't exceed the end of the file
+			//If the file is 100 bytes and the file pattern is 10 bytes, the last byte we want to check is
+			//90 -> i.e. we will check index 90, 91, 92, 93, 94, 95, 96, 97, 98, 99 and index 99 is the last
+			//index in the file bytes
+			int TraversableLength = FileBytes.Length - StartOfFilePattern.Length;
+
+			for (int i = 0; i <= TraversableLength; i++)
+			{
+				bool Match = true;
+
+				//Test the next run of characters to see if they match
+				for (int j = 0; j < StartOfFilePattern.Length; j++)
+				{
+					//If the character doesn't match, break out
+					//We're making sure that i + j doesn't exceed the length as part
+					//of the loop bounds
+					if (FileBytes[i + j] != StartOfFilePattern[j])
+					{
+						Match = false;
+						break;
+					}
+				}
+
+				//If we did find a run of
+				if (Match == true)
+				{
+					StartIndexes.Add(i);
+					i += StartOfFilePattern.Length;
+				}
+			}
+
+			//In case the pattern doesn't match, just start from the beginning of the file
+			if (!StartIndexes.Any())
+			{
+				StartIndexes.Add(0);
+			}
+
+			List<byte[]> Chunks = new List<byte[]>();
+
+			for (int i = 0; i < StartIndexes.Count; i++)
+			{
+				int Start = StartIndexes.ElementAt(i);
+				int Length = 0;
+
+				if (i + 1 == StartIndexes.Count)
+				{
+					Length = FileBytes.Length - Start;
+				}
+				else
+				{
+					Length = StartIndexes.ElementAt(i + 1) - i;
+				}
+
+				Chunks.Add(FileBytes.Skip(Start).Take(Length).ToArray());
+			}
+
+			string tempFileName = Path.GetTempFileName();
+
+			using (FileStream decompressedFileStream = File.Create(tempFileName))
+			{
+				foreach (byte[] Chunk in Chunks)
+				{
+					using (MemoryStream MStream = new MemoryStream(Chunk))
+					{
+						using (GZipStream GZStream = new GZipStream(MStream, CompressionMode.Decompress))
+						{
+							GZStream.CopyTo(decompressedFileStream);
+						}
+					}
+				}
+			}
+
+			return tempFileName;
+		}
+
+	}
+}
